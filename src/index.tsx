7d4d3424f4a46174f666fef9a319fc6823584ccc
savio591 <savio591@hotmail.com>
2021-03-25 22:17:49 -0300

    node env init - react/babel/webpack/typescript init

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import Dashboard from "./Dashboard";
+
+render(<Dashboard />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
+      port: 9000
+    },
+
+   
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          }
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader' },
+          ]
+        },
+        {
+          test: /.*\.(gif|png|jpe?g)$/i,
+          use: {
+            loader: 'file-loader',
+          }
+        }
+      ]
+    },
+  };
